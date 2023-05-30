5dc1ac911c18e55dbe93ab058ca6b109b8927b4d
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-05-30 20:00:00 +0200

    Paramétrage de devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.default_url_options = { host: 'airbnb-sabrina-travail-lewagon.herokuapp.com' }
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
