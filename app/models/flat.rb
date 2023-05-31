ec883f85dd92ff3dff0dc80ca159d428e1743373
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-05-31 12:29:52 +0200

    ajout model favorite, et ses liaisons dans flat et user

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,6 @@ class Flat < ApplicationRecord
   belongs_to :equipment
   belongs_to :category
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
+  has_many :users, through: :reviews
+  has_many :users, through: :favorites
 end
