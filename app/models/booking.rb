709dae64d08526c3d0a3c6ed43c6a8bb33e3a77d
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-05-31 19:50:51 +0200

    Ajout et validation Booking OK

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :flat
-  belongs_to :owner, class_name: "User", foreign_key: "customer_id"
+  belongs_to :customer, class_name: "User", foreign_key: "user_id"
   validates :date_arrival, :date_departure, presence: true
 end
