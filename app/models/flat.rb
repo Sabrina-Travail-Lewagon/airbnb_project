07ad81a67bca051189785a82031f1bfd3002501d
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-06-01 20:19:50 +0200

    Modif du model Flat pour ajout attach photo

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -4,4 +4,5 @@ class Flat < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :users, through: :reviews
   has_many :users, through: :favorites
+  has_many_attached :photos
 end
