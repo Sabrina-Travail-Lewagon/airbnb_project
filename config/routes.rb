1c9f5040e03ac33906539b726dafe722fb07a12a
Mickael Souchet <souchet.mickael@gmail.com>
2023-06-02 11:17:23 +0200

    flat controller (index, show), routes et changement homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
   devise_for :users
-  root to: "pages#home"
+  root to: "flats#index"
+  resources :flats, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
