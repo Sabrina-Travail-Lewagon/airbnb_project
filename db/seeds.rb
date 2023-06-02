c7e21ae52399c57b34d1ce894c256800bfb506e3
Hugo Flamigni <hugoflam@gmail.com>
2023-06-02 13:34:09 +0200

    Changed the lat & lon to float and tackled user problem in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'json'
 require 'open-uri'
+require 'faker'
 # On va chercher le fichier
+
+
 filepath = "./app/assets/files/dataset_airbnb.json"
 
 # On lit le fichier
@@ -24,23 +27,25 @@ data = JSON.parse(serial_airbnb)
 
 puts "Cleaning database..."
 Flat.destroy_all
-# puts "Creating Equipments"
-# # On va remplir les équipements
-# equipements = ['Wifi', 'Machine à laver Disco', 'Lave vaisselle sanglant', 'poupée Chucky', 'Clown', 'Serviettes Serial killer', 'Couvertures']
-# equipements.each do |equipement|
-#   equipement = Equipment.new(name: equipement)
-# end
-# equipement.save
-# puts "Equipments Done!"
 
-# puts "Creating Categories"
-# # On va remplir les équipements
-# categories = ['Bords de mer', 'Maisons hantées', 'Campagne', 'bateaux', 'Chateau hanté', 'Maison à crime']
-# categories.each do |category|
-#   category = Category.new(name: category)
-# end
-# categorie.save
-# puts "Categories Done!"
+puts "Creating Equipments"
+# On va remplir les équipements
+equipements = ['Wifi', 'Machine à laver Disco', 'Lave vaisselle sanglant', 'poupée Chucky', 'Clown', 'Serviettes Serial killer', 'Couvertures']
+equipements.each do |equipement|
+  equipement = Equipment.new(name: equipement)
+  equipement.save
+end
+
+puts "Equipments Done!"
+
+puts "Creating Categories"
+# On va remplir les équipements
+categories = ['Bords de mer', 'Maisons hantées', 'Campagne', 'bateaux', 'Chateau hanté', 'Maison à crime']
+categories.each do |category|
+  category = Category.new(name: category)
+  category.save
+end
+puts "Categories Done!"
 
 puts "Creating flats..."
 puts "Creating seed"
@@ -49,6 +54,7 @@ data.each do |item|
   # On va charger un équipement par défaut et une catégorie par défaut
   equipment = Equipment.find_or_create_by(name: 'Default Equipment')
   category = Category.find_or_create_by(name: 'Default Category')
+
   # On va récupérer les photos, qu'on stocke dans un tableau
   picture_urls = item['photos']
   # On instancie le flat
@@ -63,7 +69,7 @@ data.each do |item|
     latitude: item['location']['lat'],
     equipment: equipment,
     category: category,
-    user_id: 1
+    owner: User.last
   )
   # On va itérer sur le tableau des photos et les attachés
   picture_urls.each do |picture_url|
@@ -73,7 +79,7 @@ data.each do |item|
   end
 
   # Sauvegarder l'objet Flat
-  flat.save
+  flat.save!
 end
 
 puts "Flats Done"
