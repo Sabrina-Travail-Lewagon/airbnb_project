49a8630aff951c0ddf95d1c478dacafe51595b47
Hugo Flamigni <hugoflam@gmail.com>
2023-06-03 17:43:47 +0200

    Add search by date feature

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,29 @@
 class FlatsController < ApplicationController
   def index
+    # on définit par défaut toute la liste des flats quand la page charge
     @flats = Flat.all
-      if params[:search].present?
-        @flats = Flat.search_by_title_and_address(params[:search])
-        # @flats = @flats.where("title ILIKE ?", "%#{params[:search]}%")
-      end
+    # si l'utilisateur cherche seulement par mots clés, alors on lance la recherhce simple par mot clé
+    if params[:search].present? && (params[:arrival].first == "" || params[:departure].first == "")
+      @flats = Flat.search_by_title_and_address(params[:search])
+    # si l'utilisateur cherche par mot clé et par date
+    elsif params[:arrival].present? && params[:departure].present? && params[:search].present?
+      # d'abord on cherche tous les appartement qui n'ont pas de bookings pendant les dates recherchées
+      sql_subquery = <<~SQL
+        :arrival <= bookings.date_arrival AND :arrival >= bookings.date_departure
+        OR :departure >= bookings.date_departure AND :departure <= bookings.date_arrival
+      SQL
+      @flats = @flats.joins(:bookings).where.not(
+        sql_subquery, arrival: params[:arrival], departure: params[:departure], search: params[:search]
+        # puis on refiltre le résultat en fonction des mots clés entrés par l'utilisateur
+      ).where("address ILIKE :query OR title ILIKE :query", query: "%#{params[:search]}%")
+    # si l'utilisateur cherche par date on cherche tous les appartement qui n'ont pas de bookings pendant les dates recherchées
+    elsif params[:arrival].present? && params[:departure].present?
+      sql_subquery = <<~SQL
+        :arrival >= bookings.date_arrival AND :arrival <= bookings.date_departure
+        OR :departure <= bookings.date_departure AND :departure >= bookings.date_arrival
+      SQL
+      @flats = @flats.joins(:bookings).where.not(sql_subquery, arrival: params[:arrival], departure: params[:departure])
+    end
   end
 
   def show
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -4,9 +4,11 @@ class Flat < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_many :users, through: :reviews
   has_many :users, through: :favorites
+  has_many :bookings
   has_many_attached :photos
 
   include PgSearch::Model
+
 
   pg_search_scope :search_by_title_and_address,
                   against: [:title, :address],
