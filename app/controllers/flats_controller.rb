0ee8dc907e564e903f6df737939a5a72b7888b34
Hugo Flamigni <hugoflam@gmail.com>
2023-06-03 12:02:04 +0200

    ading the searchbar and config search by keyword

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,10 @@
 class FlatsController < ApplicationController
   def index
     @flats = Flat.all
+      if params[:search].present?
+        @flats = Flat.search_by_title_and_address(params[:search])
+        # @flats = @flats.where("title ILIKE ?", "%#{params[:search]}%")
+      end
   end
 
   def show
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -5,4 +5,12 @@ class Flat < ApplicationRecord
   has_many :users, through: :reviews
   has_many :users, through: :favorites
   has_many_attached :photos
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_address,
+                  against: [:title, :address],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
