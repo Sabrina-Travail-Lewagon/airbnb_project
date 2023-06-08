dedc5309c278d24b2560afd137f10c38b6039a42
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-06-08 10:39:20 +0200

    modif-Booking-addColumnStatus-et-autres

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :customer, class_name: "User", foreign_key: "user_id"
   validates :date_arrival, :date_departure, presence: true
   validates :date_departure, comparison: { greater_than: :date_arrival }
+  # Status ne peut prendre que les valeurs : Cancelled, Pendinf et Approuved
+  validates :status, inclusion: { in: ['Cancelled', 'Pending', 'Approuved']}
 end
