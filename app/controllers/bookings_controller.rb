fb8ee8f677c26f415e6902485d06575a65bc15f7
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-06-09 20:35:17 +0200

    modif status et creation de controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,7 +12,7 @@ class BookingsController < ApplicationController
     @booking.customer = @user
     @booking.status = "Pending" # Par défaut tout booking est en attente
     if @booking.save
-      redirect_to my_bookings_path(current_user)
+      redirect_to bookings_path
     else
       render flat_path(@flat), status: :unprocessable_entity
     end
@@ -20,7 +20,17 @@ class BookingsController < ApplicationController
 
   def index
     @user = current_user
-    @bookings = Booking.where(customer: @user)
+    @bookings = @user.bookings
+    # Equivalent à la ligne du dessous :
+    # @bookings = Booking.where(customer: @user)
+  end
+
+  def approved
+    @booking = Booking.find(params[:id])
+    @booking.status = "Approved"
+    if @booking.save
+      redirect_to my_flat_path(@booking.flat)
+    end
   end
 
   private
